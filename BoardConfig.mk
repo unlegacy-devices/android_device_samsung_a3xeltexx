76be01ff4e7f20e337a2ec65afe36a73b4f16a23
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-12-22 16:34:16 +0100

    Add TWRP support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,5 +68,16 @@ BOARD_MODEM_TYPE := tss310
 TARGET_GLOBAL_CFLAGS += -DANDROID_MULTI_SIM
 TARGET_GLOBAL_CPPFLAGS += -DANDROID_MULTI_SIM
 
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_NO_BOOTLOADER := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_EXCLUDE_NANO := true
+TW_EXCLUDE_BASH := true
+RECOVERY_SDCARD_ON_DATA := true
 # inherit from the proprietary version
 -include vendor/samsung/a3xe3gxx/BoardConfigVendor.mk
